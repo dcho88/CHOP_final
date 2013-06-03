c24802669066f4be708dac3199dc8230dfaca401
Dave <azndavecho@gmail.com>
2013-06-03 16:27:38 -0400

    creation of database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CHOPFinal::Application.config.secret_token = 'a84af559b7446116908336096827adab1adb731082de5ea59a31bc6c7479a7dad4db5939d5bd6f9ae59e17780c40ca30b3503605e7a35cbc3c9ce50b02a85575'
